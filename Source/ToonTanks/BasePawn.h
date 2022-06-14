57927471c779dd5afb9c122546bc541405316a78
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2022-06-14 16:14:31 +0100

    Blueprint access to speed

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Speed = 400.f;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
