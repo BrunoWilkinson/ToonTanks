2b6666aac6f35e3f5da0433ec469fe25fd96f043
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2022-06-17 13:15:47 +0100

    Apply Damage

--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -30,6 +30,9 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Combat")
 	class UProjectileMovementComponent* ProjectileMovementComponent;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float Damage = 50.f;
+
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
