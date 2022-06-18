e6453c9cd1f122dbeb422564b53da63dae20b958
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2022-06-18 18:22:52 +0100

    Smoke Trail

--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -32,6 +32,8 @@ private:
 	float Damage = 50.f;
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	class UParticleSystem* HitParticles;
+	UPROPERTY(VisibleAnywhere, Category = "Combat")
+	class UParticleSystemComponent* ParticleSystemComponent;
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
