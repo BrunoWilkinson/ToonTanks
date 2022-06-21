65cc1fdec578496a0577eb7c941360472fa5d17f
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2022-06-21 13:58:33 +0100

    Polish

--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -19,11 +19,11 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 	void HandleDestruction();
 	APlayerController* GetTankPlayerController() const { return TankPlayerController; };
+	bool bAlive = true;
 
 protected:
 	// Called when the game starts or when spawned
